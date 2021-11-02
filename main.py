b33d2e7a9ca8145f29543221a694fd43c575ca42
superpooop <c1014088@newcast;e.ac.uk>
2021-11-02 11:24:27 +0000

    added way to search through contacts

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# class definition
+AddContact = input("Do you want to add a contact? (Y/N): ")
+
+#class definition
 class Contact:
 
     # constructor
@@ -8,11 +10,35 @@ class Contact:
         self.phonenumber = phonenumber
         self.birthdate = birthdate
 
-if __name__== "__main__":
+#adds contact adding
+while AddContact == "Y" :
 
-    c = str(Contact(input(str("Enter Name: ")), input(str("Enter Address: ")), input(str("Enter Phonenumber: ")), input(str("Enter Birthdate:"))))
-    t = (c.name.append, c.address, c.phonenumber, c.birthdate)
+    c = Contact(input("Enter Name: "), input("Enter Address: "), input("Enter Phonenumber: "), input("Enter Birthdate: "))
+    t = (c.name, c.address, c.phonenumber, c.birthdate)
 
-    f = open("database.txt")
-    f.write(t)
-    f.close()
+    f = open("database.txt", "a")
+    f.write('\n' + str(t))
+    f.close
+
+    AddContact = input("Do you want to add a contact?: ")
+
+
+else:
+    SearchContact = input("Do you want to search through contacts? (Y/N): ")
+
+
+while SearchContact == "Y":
+    search1 = input("What would you like to search for?: ")
+    
+##reference : https://stackoverflow.com/questions/18366554/how-to-search-for-word-in-text-file-and-print-part-of-line-with-python
+    
+    # opening a text file
+    f1 = open("database.txt", "r")
+
+    #searching for the line
+    for line in f1:
+        if search1 in line:
+            print(line)
+
+
+    f1.close()
